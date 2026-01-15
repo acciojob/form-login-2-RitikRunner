1e89c783161cb0888ef1550f74dcb31ba104c302
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-16 01:54:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let submit = document.getElementById("submit")
+let fn = document.getElementById("fn")
+let ln = document.getElementById("ln")
+let pn = document.getElementById("nn")
+let ei = document.getElementById("ei")
+submit.addEventListener("click",()=>{
+	alert("First Name:" fn.value, "Last Name:" ln.value,
+		 "Phone Number:" pn.value, "Email ID:" ei.value)
+})
